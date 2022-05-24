b2aab917b4fb400085545d9b97350ef575cdc13e
RjRaju <50709168+RjRaju143@users.noreply.github.com>
2022-05-24 13:18:01 +0530

    Create randomMacChanger.sh

---
+++ randomMacChanger.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+RED="\e[31m"
+GREEN="\e[32m"
+
+if [[ $UID = 0 ]]; then
+    if [[ -f /usr/bin/macchanger ]]; then
+        macchanger -l > vendormac.txt
+        ouimac=$(shuf -n 1 vendormac.txt | awk '{print$3}')
+        uaama=$(printf '%02x:%02x:%02x' $[Random%256] $[Random%256] $[Random%256])
+        read -p "Enter your network interface : " IMAC
+        ifconfig $IMAC down
+        macchanger -m "$ouimac:$uaama" $IMAC
+        ifconfig $IMAC up
+        rm -rf vendormac.txt
+    else
+        echo "Macchanger not found "
+        read -p "Do you want to install (y/N) " SELECT
+        if [[ $SELECT == y || $SELECT == Y || $SELECT == Yes || $SELECT == yes || $SELECT == YES ]]; then
+            apt install -y macchanger
+        elif [[ $SELECT == n || $SELECT == N || $SELECT == no || $SELECT == No ]]; then
+            printf "${GREEN}exit..."
+        else
+            printf "${GREEN}exit"
+        fi
+    fi
+else
+    echo -e "${RED}run as root"
+fi
